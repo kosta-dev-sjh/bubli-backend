ccca41e03483bdea0ec1da03f8b73ef7e04ae048
Song991123 <sjh11235678@gmail.com>
2026-06-25 10:33:05 +0900

    fix: move chat read sequence migration to v2

--- src/main/resources/db/migration/V2__chat_message_client_id_scope.sql
+++ src/main/resources/db/migration/V2__chat_message_client_id_scope.sql
@@ -3,3 +3,6 @@ ALTER TABLE chat_messages
 
 ALTER TABLE chat_messages
     ADD CONSTRAINT uk_chat_messages_room_client_message UNIQUE (chat_room_id, client_message_id);
+
+ALTER TABLE chat_room_members
+    ADD COLUMN last_read_sequence BIGINT;
