e1b6f04f6742f205fe410dd99781170d353e92d9
jerygen <myksphone2001@gmail.com>
2026-07-04 16:30:52 +0900

    feat: 개인 자료 후보 승인 시 개인 TODO 생성
    - roomId 없는 TASK/TODO 후보 승인 시 개인 TODO로 반영
    
    - TaskPublicService에 개인 TODO 생성 경계 추가
    
    - 에이전트 후보 승인 반영 테스트 보강

--- src/main/java/com/bubli/work/task/service/TaskPublicService.java
+++ src/main/java/com/bubli/work/task/service/TaskPublicService.java
@@ -1,7 +1,8 @@
 package com.bubli.work.task.service;
 
-import com.bubli.work.task.dto.TaskResult;
+import com.bubli.work.task.dto.CreatePersonalTaskCommand;
 import com.bubli.work.task.dto.CreateRoomTaskCommand;
+import com.bubli.work.task.dto.TaskResult;
 
 import java.time.Instant;
 import java.util.List;
@@ -20,6 +21,8 @@ public interface TaskPublicService {
 	boolean existsByWbsItemId(UUID wbsItemId);
 
 	void assertNoTaskLinkedToWbsItem(UUID wbsItemId);
+
+	TaskResult createPersonalTask(UUID userId, CreatePersonalTaskCommand command);
 
 	TaskResult createRoomTask(UUID userId, UUID roomId, CreateRoomTaskCommand command);
 }
--- src/main/java/com/bubli/work/task/service/TaskPublicServiceImpl.java
+++ src/main/java/com/bubli/work/task/service/TaskPublicServiceImpl.java
@@ -3,6 +3,7 @@ package com.bubli.work.task.service;
 import com.bubli.global.error.BusinessException;
 import com.bubli.global.error.ErrorCode;
 import com.bubli.project.service.ProjectMembershipPublicService;
+import com.bubli.work.task.dto.CreatePersonalTaskCommand;
 import com.bubli.work.task.dto.CreateRoomTaskCommand;
 import com.bubli.work.task.dto.TaskResult;
 import com.bubli.work.task.entity.Task;
@@ -69,6 +70,19 @@ public class TaskPublicServiceImpl implements TaskPublicService {
 		if (existsByWbsItemId(wbsItemId)) {
 			throw new BusinessException(ErrorCode.WORK_400_001);
 		}
+	}
+
+	@Override
+	@Transactional
+	public TaskResult createPersonalTask(UUID userId, CreatePersonalTaskCommand command) {
+		Task task = Task.createPersonal(
+				userId,
+				command.title(),
+				command.description(),
+				command.status(),
+				command.dueAt()
+		);
+		return TaskResult.from(taskRepository.save(task));
 	}
 
 	@Override
